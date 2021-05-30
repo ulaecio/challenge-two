a1466b83b0a4da30dc36675a8b6f5bb17bd0675f
ulaecio <lenssonpol@hotmail.com>
2021-05-30 12:40:40 -0300

    Create App, Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+const Navbar = () => {
+    return (
+    <nav className="nav-bar">
+        <div className="col-1">
+            <h1>Github localizador</h1>
+        </div>
+    </nav>
+    );
+}
+
+export default Navbar;
